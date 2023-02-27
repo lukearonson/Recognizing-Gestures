5467718a2ccd4473bc902ec734425be7f4e9a3e6
Joncode9001 <jaytuby@gmail.com>
2023-02-27 09:12:04 -0800

    Added files
    Added RotateView and DragView files

---
+++ Recognizing Gestures/SingleLine.swift
@@ -0,0 +1,20 @@
+//
+//  SingleLine.swift
+//  Recognizing Gestures
+//
+//  Created by Jonah Reinis on 2/27/23.
+//
+
+import SwiftUI
+
+struct SingleLine: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SingleLine_Previews: PreviewProvider {
+    static var previews: some View {
+        SingleLine()
+    }
+}
