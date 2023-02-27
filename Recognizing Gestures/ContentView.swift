2f153fddc7c6abf773ecc041a0581fe66c1bf48a
lukearonson <126193307+lukearonson@users.noreply.github.com>
2023-02-27 09:21:39 -0800

    Added ContentView and GestureRow

--- Recognizing Gestures/ContentView.swift
+++ Recognizing Gestures/ContentView.swift
@@ -9,15 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationSplitView{
+            List{
+                NavigationLink(destination: TapView()) {
+                    GestureRow(title: "Tap", description: "Tap a shape to interact with it.", systemImage: "hand.point.up.left")
+                }
+                NavigationLink(destination: LongPressView()) {
+                    GestureRow(title: "Touch and Hold", description: "Touch and hold shape to interact with it.", systemImage: "hand.tap")
+                }
+                NavigationLink(destination: DragView()) {
+                    GestureRow(title: "Drag", description: "Drag a shape using one finger.", systemImage: "arrow.up.and.down.and.arrow.left.and.right")
+                }
+                NavigationLink(destination: RotateView()) {
+                    GestureRow(title: "Rotate", description: "Rotate shape with two fingers.", systemImage: "arrow.triangle.2.circlepath")
+                }
+                NavigationLink(destination: SingleLine()) {
+                    GestureRow(title: "Line Drawing", description: "Use finger to draw a simple line shape.", systemImage: "pencil.tip")
+                }
+            }
+            .navigationTitle("Gestures")
+        }detail: {
+            Text("Select a Gesture")
+                .foregroundColor(.secondary)
+                }
+            }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
