a68212cb253f79060bb6fc3f75e745cbf5530db1
Joncode9001 <jaytuby@gmail.com>
2023-02-27 09:20:25 -0800

    added singleline

--- Recognizing Gestures/SingleLine.swift
+++ Recognizing Gestures/SingleLine.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct SingleLine: View {
+    @State var lineStart = CGPoint.zero
+    @State var lineEnd = CGPoint.zero
+    var lineDrawingGesture: some Gesture
+    {
+        DragGesture()
+            .onChanged{value in
+                lineStart = value.startLocation
+                lineEnd = value.location
+            }
+            .onEnded
+        {
+            value in
+            lineEnd = value.location
+        }
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack
+        {
+            Text("Touch and drag to make a line")
+            Spacer()
+            Path
+            {
+                path in
+                path.move(to: lineStart)
+                path.addLine(to: lineEnd)
+            }
+            .stroke(Color.green, lineWidth: 8.0)
+            .contentShape(Rectangle())
+            .gesture(lineDrawingGesture)
+        }
+        .navigationTitle("Line Drawing")
+        .padding()
+        .toolbar
+        {
+            Button("Reset")
+            {
+                lineStart = .zero
+                lineEnd = .zero
+            }
+        }
     }
 }
 
