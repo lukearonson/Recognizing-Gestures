ff00cbac8c265b73f154713606d2b349ccbff68f
lukearonson <126193307+lukearonson@users.noreply.github.com>
2023-02-27 08:43:42 -0800

    Initial Commit

---
+++ Recognizing Gestures/Recognizing_GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recognizing_GesturesApp.swift
+//  Recognizing Gestures
+//
+//  Created by Luke Aronson on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Recognizing_GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
